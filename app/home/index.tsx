51a843bb7976e97a90c3a31150ef6e59aa9b761f
Ezequiel <zapata.ed1989@gmail.com>
2024-12-15 21:21:28 -0300

    categories flats

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,13 @@
-import { View, Text, ActivityIndicator } from 'react-native'
+import { View, Text, ActivityIndicator, ScrollView } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useMovies } from '@/presentation/hooks/useMovies'
 import MainSlideShow from '@/presentation/components/movies/MainSlideShow';
+import MovieHorizontalList from '@/presentation/components/movies/MovieHorizontalList';
 
 const HomeScreen = () => {
 
   const safeArea = useSafeAreaInsets()
-  const { nowPlayingQuery } = useMovies();
+  const { nowPlayingQuery, popularQuery, topRatedQuery, upcomingQuery } = useMovies();
 
 
   if (nowPlayingQuery.isLoading) {
@@ -23,12 +24,39 @@ const HomeScreen = () => {
 
 
   return (
-    <View className='mt-2' style={{ paddingTop: safeArea.top }}>
-      <Text className='text-3xl font-bold px-4 mb-2'>MovieApp</Text>
+    <ScrollView >
+      <View className='mt-2 pb-10' style={{ paddingTop: safeArea.top }}>
+        <Text className='text-3xl font-bold px-4 mb-2'>MovieApp</Text>
 
-      {/* Carousel */}
-      <MainSlideShow movies={ nowPlayingQuery.data ?? [] }/>
-    </View>
+        {/* Carousel */}
+        <MainSlideShow movies={nowPlayingQuery.data ?? []} />
+
+        {/* Populares */}
+        <MovieHorizontalList
+          title='Populares'
+          movies={popularQuery.data ?? []}
+          className='mb-5'
+        />
+        {/* Top Rated */}
+        <MovieHorizontalList
+          title='Mejor calificados'
+          movies={topRatedQuery.data ?? []}
+          className='mb-5'
+        />
+        {/* Upcoming */}
+        <MovieHorizontalList
+          title='Proximamente'
+          movies={upcomingQuery.data ?? []}
+          className='mb-5'
+        />
+        {/* Upcoming */}
+        <MovieHorizontalList
+          title='Proximamente'
+          movies={upcomingQuery.data ?? []}
+          className='mb-5'
+        />
+      </View>
+    </ScrollView>
   )
 }
 
--- presentation/hooks/useMovies.ts
+++ presentation/hooks/useMovies.ts
@@ -1,19 +1,43 @@
 import { nowPlayingAction } from "@/core/actions/movie/now-playing.action"
+import { popularMoviesAction } from "@/core/actions/movie/popular.action"
+import { topRatedMoviesAction } from "@/core/actions/movie/top-rated.action"
+import { upcomingMoviesAction } from "@/core/actions/movie/upcoming.action"
 import { useQuery } from "@tanstack/react-query"
 
 export const useMovies = () => {
 
     // Queries
     const nowPlayingQuery = useQuery({
-        queryKey: ['movies','nowPlaying'],
+        queryKey: ['movies', 'nowPlaying'],
         queryFn: nowPlayingAction,
+        staleTime: 1000 * 60 * 60 * 24 // 24hs
+    })
+
+    const popularQuery = useQuery({
+        queryKey: ['movies', 'popular'],
+        queryFn: popularMoviesAction,
+        staleTime: 1000 * 60 * 60 * 24 // 24hs
+    })
+    
+    const topRatedQuery = useQuery({
+        queryKey: ['movies', 'top-rated'],
+        queryFn: topRatedMoviesAction,
+        staleTime: 1000 * 60 * 60 * 24 // 24hs
+    })
+    
+    const upcomingQuery = useQuery({
+        queryKey: ['movies', 'upcomig'],
+        queryFn: upcomingMoviesAction,
         staleTime: 1000 * 60 * 60 * 24 // 24hs
     })
 
 
 
     return {
-        nowPlayingQuery
+        nowPlayingQuery,
+        popularQuery,
+        topRatedQuery,
+        upcomingQuery,
     }
 
 
